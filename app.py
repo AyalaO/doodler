5a86205110983504fcdcc152d86bc6df85763680
Ayala Ohyon <ayala_ohayon@hotmail.com>
2025-05-28 15:47:01 +0200

    add logo

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-# st.sidebar.image("imgs/logo_valident.png")
+st.sidebar.image("imgs/logo.png")
 st.sidebar.write("")
 st.sidebar.write("")
 my_upload = st.sidebar.file_uploader("")
@@ -17,4 +17,4 @@ my_upload = st.sidebar.file_uploader("")
 # set-up main page
 if my_upload is not None:
     # read data
-    df = pd.read_excel(my_upload)
+    df = pd.read_excel(my_upload) 
